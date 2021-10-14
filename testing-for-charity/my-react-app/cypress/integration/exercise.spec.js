5d0e69385809ac6658fa75ce8239da55ac1a2805
Jessica Blankemeier <jblankemeier@tandemdiabetes.com>
2021-10-14 09:49:39 -0700

    complete cypress excercise

--- testing-for-charity/my-react-app/cypress/integration/exercise.spec.js
+++ testing-for-charity/my-react-app/cypress/integration/exercise.spec.js
@@ -2,28 +2,17 @@
 
 describe('Exercise', () => {
     it('loads', ()=> {
-        /** Your code below */
-    
-        //1. Use cy.visit('/') to go to the app url
-        //2. Use cy.get('.App-link').should('be.visible') to assert valid state
-        /** Your code above */
+        cy.visit('/')
+        cy.get('.App-link').should('be.visible')
     })
     
     it('link goes to ultimateqa', ()=> {
-        /** Your code below */
-    
-        // 1. Use cy.visit('/') to go to the app url
-        // 2. cy.get('').should('have.attr', 'href').and('include', 'ultimateqa.com')
-    
-        /** Your code above */
+        cy.visit('/')
+        cy.get('.App-link').should('have.attr', 'href').and('include', 'ultimateqa.com')
     })
     
     it('should open link in new tab',()=> {
-        /** Your code below */
-    
-        // 1. Cmon, you know how to open the url now :)
-        // 2. cy.get('').should('have.attr', 'WHAT').and('include', 'WHAT VALUE')
-    
-        /** Your code above */
+        cy.visit('/')
+        cy.get('.App-link').should('have.attr', 'target').and('include', 'blank')
     })  
 })
